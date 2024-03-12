35b0b9201d75cb96071d5410f4f0aa268a3a05f5
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-12 21:13:09 +0100

    Add usestate for toggle, onclick event listener, change icon depending on state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,29 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="w-full flex py-6 justify-between items-center">
       <img src={logo} alt="Hoobank" className="w-[124px] h-[32px]" />
-      <ul className="list-none hidden sm:flex justify-end items-center flex-1">
+      <ul className="hidden sm:flex justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
-          <li key={nav.id} className={`font-poppins font-normal text-[16px] text-white`}>
-            <a href={`#${nav.id}`}>
-              {nav.title}
-            </a>
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal text-[16px] text-white ml-10`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
-        )
-        
-        )}
+        ))}
       </ul>
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={ () => setToggle((prev) => !prev)}
+        />
+      </div>
     </nav>
   );
 };
