cdaee2f5dd60a942a6bf21525a9c3dd6cfd83765
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-12 20:57:41 +0100

    Add logo, navlinks and navlink styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="w-full flex py-6 justify-between items-center">
+      <img src={logo} alt="Hoobank" className="w-[124px] h-[32px]" />
+      <ul className="list-none hidden sm:flex justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li key={nav.id} className={`font-poppins font-normal text-[16px] text-white`}>
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        )
+        
+        )}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
