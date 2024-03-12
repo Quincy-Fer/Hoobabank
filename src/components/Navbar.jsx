1be8f045f197ac9eb84f1768f0083cb17a533d01
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-12 21:32:17 +0100

    Create Responsive Hamburger Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className="w-full flex py-6 justify-between items-center">
+    <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="Hoobank" className="w-[124px] h-[32px]" />
       <ul className="hidden sm:flex justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
@@ -23,8 +23,24 @@ const Navbar = () => {
           src={toggle ? close : menu}
           alt="menu"
           className="w-[28px] h-[28px] object-contain"
-          onClick={ () => setToggle((prev) => !prev)}
+          onClick={() => setToggle((prev) => !prev)}
         />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 my-2 w-full sidebar`}
+        >
+          <ul className="flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal text-[16px] text-white mb-4`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
