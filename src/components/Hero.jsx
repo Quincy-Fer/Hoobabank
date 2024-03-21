0669d3df752f82b64371973a9e995ef42c30e6bf
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-21 13:30:47 +0100

    Create Getstarted component, nicely styled div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import styles from "../style";
 import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
 
 const Hero = () => {
   return (
@@ -13,10 +14,19 @@ const Hero = () => {
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
-            <span className="text-white">20%</span> Discount for 
+            <span className="text-white">20%</span> Discount for
             <span className="text-white"> 1 month</span> Account
           </p>
         </div>
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            The Next <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Generation</span>{" "}
+          </h1>
+        </div>
+        <div className="ss:flex hidden md:mr-4 mr-0">
+            <GetStarted />
+          </div>
       </div>
     </section>
   );
