0e74db5169002af9ad61c17161914e9e2c1f30f8
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-21 16:57:44 +0100

    Cleanup formatting

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -10,11 +10,8 @@ const Billing = () => {
           alt="billing"
           className="w-[100%] h-[100%] relative z-[5]"
         />
-
-        {/* gradient start */}
         <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
         <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
-        {/* gradient end */}
       </div>
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>
@@ -27,8 +24,16 @@ const Billing = () => {
           aspernatur?
         </p>
         <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
-          <img src={apple} alt="app store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"/>
-          <img src={google} alt="google store" className="w-[128px] h-[42px] object-contain  cursor-pointer"/>
+          <img
+            src={apple}
+            alt="app store"
+            className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+          />
+          <img
+            src={google}
+            alt="google store"
+            className="w-[128px] h-[42px] object-contain  cursor-pointer"
+          />
         </div>
       </div>
     </section>
