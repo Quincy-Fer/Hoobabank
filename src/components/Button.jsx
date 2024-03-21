f197c82d99b3209b3792aed0cb06b3a859556fb9
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-21 18:17:50 +0100

    Import assets styles and Button component, create left div

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ styles }) => {
     <button
       type="button"
       className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px]
-       text-primary outline-none ${styles} rounded-2xl`}
+       text-primary outline-none ${styles} rounded-xl`}
     >
       Get Started
     </button>
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,5 +1,25 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
 const CardDeal = () => {
-  return <div>CardDeal</div>;
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className="sm:block hidden" />
+          in a few steps
+        </h2>
+        <p className={`${styles.paragraph} w-[470px] mt-5`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          molestiae aspernatur quae amet illo, ab voluptates nemo, suscipit
+          culpa cum, id doloribus architecto recusandae neque. Saepe explicabo
+          cumque perspiciatis fugit!
+        </p>
+        <Button styles="mt-10" />
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
