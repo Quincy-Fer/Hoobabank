5d9393191209dbbafa839f077f15443e3bbecac9
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-21 16:16:39 +0100

    Import styles, style two divs containing icon and text

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,9 +1,13 @@
+import styles from "../style";
 
-
-const FeatureCard = ({icon, title, content, index}) => {
+const FeatureCard = ({ icon, title, content, index }) => {
   return (
-    <div><img src={icon} alt="" /></div>
-  )
-}
+    <div className="flex flex-row p-6 rounded-[20px] mb-6 feature-card">
+      <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+        <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+      </div>
+    </div>
+  );
+};
 
-export default FeatureCard
+export default FeatureCard;
