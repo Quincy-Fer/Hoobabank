b710971b01d70b7e757b07e30462a2dea7406310
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-22 11:09:02 +0100

    Link home in nav to navbar instead of hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className="w-full flex py-6 justify-between items-center navbar">
+    <nav className="w-full flex py-6 justify-between items-center navbar" id="home">
       <img src={logo} alt="Hoobank" className="w-[124px] h-[32px]" />
       <ul className="hidden sm:flex justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
