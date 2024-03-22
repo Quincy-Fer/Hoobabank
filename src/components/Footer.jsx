49740e31866ffdfcae5c9da8b1f6919ee95aff71
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-22 15:42:17 +0100

    Footer section layout import logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,21 @@
+import styles from "../style";
+import { logo } from "../assets";
+import { footerLinks, socialMedia } from "../constants";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+        <div className="flex-1 flex flex-col justify-start mr-10">
+          <img
+            src={logo}
+            alt="hoobank"
+            className="w-[266px] h-[72px] object-contain"
+          />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
