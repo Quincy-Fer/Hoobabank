b383a973e076b6acfeea77333350fa0e8f3fff90
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-22 15:18:29 +0100

    Style h2 p tag. Layout divs

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -7,14 +7,14 @@ const CTA = () => {
       className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2
       rounded-[20px] box-shadow`}
     >
-      <div>
-        <h2>Let's try our service now!</h2>
-        <p>
+      <div className="flex flex-1 flex-col">
+        <h2 className={`${styles.heading2}`}>Let's try our service now!</h2>
+        <p className={`${styles.paragraph} max-w-[470px]`}>
           Everything you need to accept card payments and grow your business
           anywhere on the planet
         </p>
       </div>
-      <div>
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
         <Button />
       </div>
     </section>
