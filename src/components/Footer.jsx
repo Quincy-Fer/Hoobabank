7fb23c7d690e64904baf48cf1d2788e4cce3a449
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-22 15:51:58 +0100

    add paragraph, map over footerlinks to make 3 divs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,6 +12,14 @@ const Footer = () => {
             alt="hoobank"
             className="w-[266px] h-[72px] object-contain"
           />
+          <p className={`${styles.paragraph} mt-10 max-w-[310px]`}>
+            A new way to make the payments easy, reliable and secure
+          </p>
+        </div>
+        <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+          {footerLinks.map((footerLink) => (
+            <div key={footerLink.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]">lala</div>
+          ))}
         </div>
       </div>
     </footer>
