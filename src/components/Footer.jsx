cd694abeae3113b19fe4c2db0f57f1f34700c954
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-22 16:09:24 +0100

    Add Copyright section and socials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,16 +22,39 @@ const Footer = () => {
               key={footerLink.key}
               className="flex flex-col ss:my-0 my-4 min-w-[150px]"
             >
-              <h4 className="font-poppins font-semibold text-[18px] leading-[27px] text-white mb-6">{footerLink.title}</h4>
+              <h4 className="font-poppins font-semibold text-[18px] leading-[27px] text-white mb-6">
+                {footerLink.title}
+              </h4>
               <ul>
                 {footerLink.links.map((link, index) => (
                   <li
                     className={`font-poppins font-normal text-[16px] leading-[24px]
                      text-dimWhite hover:text-secondary cursor-pointer mb-2`}
-                  >{link.name}</li>
+                  >
+                    {link.name}
+                  </li>
                 ))}
               </ul>
             </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+        <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
+          Copyright Ⓒ 2022 HooBank. All Rights Reserved.
+        </p>
+
+        <div className="flex flex-row md:mt-0 mt-6">
+          {socialMedia.map((social, index) => (
+            <img
+              key={social.id}
+              src={social.icon}
+              alt={social.id}
+              className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+                index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+              }`}
+              onClick={() => window.open(social.link)}
+            />
           ))}
         </div>
       </div>
