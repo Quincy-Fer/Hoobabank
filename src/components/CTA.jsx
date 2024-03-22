ac40a38f38bc94ee576c8e8168d490abbf7b5734
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-22 15:14:20 +0100

    Create h2 p, style section div and import Button

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,5 +1,24 @@
+import styles from "../style";
+import Button from "./Button";
+
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <section
+      className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2
+      rounded-[20px] box-shadow`}
+    >
+      <div>
+        <h2>Let's try our service now!</h2>
+        <p>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet
+        </p>
+      </div>
+      <div>
+        <Button />
+      </div>
+    </section>
+  );
 };
 
 export default CTA;
